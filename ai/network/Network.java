3916e567b177cc8b46734e277d92b3812f9a3668
Rab7b <5myinc5@gmail.com>
2026-04-11 17:44:23 -0300

    added network class

---
+++ ai/network/Network.java
@@ -0,0 +1,33 @@
+package ai.network;
+import ai.neuron.Neuron;
+
+public class Network{
+    private Neuron[] neurons;
+    public Network(int neuronCount, int inputSize, double[] inputs, double lr) {
+        neurons = new Neuron[neuronCount];
+        for (int i = 0; i < neuronCount; i++) {
+            neurons[i] = new Neuron(inputSize, inputs, lr);
+        }
+    }
+    public double predict(double target){
+        double best = 0.0;
+        for (int i = 0; i < neurons.length; i++) {
+            double output = neurons[i].predict();
+            if (Math.abs(output - target) < Math.abs(best - target)) {
+                best = output;
+            }
+        }
+        return best;
+    }
+    public void train(double target) {
+        for (int i = 0; i < neurons.length; i++) {
+            neurons[i].train(target);
+        }
+    }
+    public void qlearn(double reward, double next, double gamma) {
+        for (int i = 0; i < neurons.length; i++) {
+            neurons[i].qlearn(reward, next, gamma);
+        }
+    }
+
+}
